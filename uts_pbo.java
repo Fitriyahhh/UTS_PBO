4966c22e0a21b5acdd6d2a97d04d19e610a9617a
Fitriyahhh <71115567+Fitriyahhh@users.noreply.github.com>
2020-09-11 12:18:43 +0700

    Add files via upload

---
+++ uts_pbo.java
@@ -0,0 +1,63 @@
+
+public class uts_pbo {
+	
+	public static void main (String []args) {
+		
+	String huruf1 = "A";
+	StringBuffer A = new StringBuffer (huruf1);
+	String huruf2 = "B";
+	StringBuffer B = new StringBuffer (huruf2);
+	String huruf3 = "C";
+	StringBuffer C = new StringBuffer (huruf3);
+	String huruf4= "D";
+	StringBuffer D = new StringBuffer (huruf4);
+	String huruf5 = "E";
+	StringBuffer E = new StringBuffer (huruf5);
+	String huruf6 = "F";
+	StringBuffer F = new StringBuffer (huruf6);
+	String huruf7 = "G";
+	StringBuffer G = new StringBuffer (huruf7);
+	String huruf8 = "H";
+	StringBuffer H = new StringBuffer (huruf8);
+	String huruf9 = "I";
+	StringBuffer I = new StringBuffer (huruf9);
+	String huruf10 = "J";
+	StringBuffer J = new StringBuffer (huruf10);
+	String huruf11 = "K";
+	StringBuffer K = new StringBuffer (huruf11);
+	String huruf12 = "L";
+	StringBuffer L = new StringBuffer (huruf12);
+	String huruf13 = "M";
+	StringBuffer M = new StringBuffer (huruf13);
+	String huruf14 = "N";
+	StringBuffer N = new StringBuffer (huruf14);
+	String huruf15 = "O";
+	StringBuffer O = new StringBuffer (huruf15);
+	String huruf16 = "P";
+	StringBuffer P = new StringBuffer (huruf16);
+	String huruf17 = "Q";
+	StringBuffer Q = new StringBuffer (huruf17);
+	String huruf18 = "R";
+	StringBuffer R = new StringBuffer (huruf18);
+	String huruf19 = "S";
+	StringBuffer S = new StringBuffer (huruf19);
+	String huruf20 = "T";
+	StringBuffer T = new StringBuffer (huruf20);
+	String huruf21 = "U";
+	StringBuffer U = new StringBuffer (huruf21);
+	String huruf22 = "V";
+	StringBuffer V = new StringBuffer (huruf22);
+	String huruf23 = "W";
+	StringBuffer W = new StringBuffer (huruf23);
+	String huruf24 = "X";
+	StringBuffer X = new StringBuffer (huruf24);
+	String huruf25 = "Y";
+	StringBuffer Y = new StringBuffer (huruf25);
+	String huruf26 = "Z";
+	StringBuffer Z = new StringBuffer (huruf26);
+	
+	
+	System.out.println(huruf6+huruf9+huruf20+huruf18+huruf9+huruf25+huruf1+
+	huruf8+"-"+huruf14+huruf21+huruf18+huruf6+huruf1+huruf9+huruf4+huruf1+huruf8);
+	}
+}
